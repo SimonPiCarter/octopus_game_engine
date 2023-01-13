b6b228b98754001f99df1db229c9e5e7d0e25aba
Simon PIERRE <simon.pierre@eurodecision.com>
2023-01-13 15:07:20 +0100

    [test] added test on move command

---
+++ src/octopus/test/src/moveCommande.test.cc
@@ -0,0 +1,83 @@
+#include <gtest/gtest.h>
+
+#include <command/EntityMoveCommand.hh>
+#include <step/entity/EntityStep.hh>
+#include <step/Step.hh>
+#include <state/State.hh>
+#include <utils/Vector.hh>
+
+///
+/// This test suite aims at checking that EntityMoveCommand works properly
+/// - Generate the correct step
+/// - terminates correctly
+/// - goes through waypoints correctly
+///
+
+using namespace octopus;
+
+///
+/// Create entity on 3,3
+/// Create move commande with way point 4,3 -> 4,4
+///
+TEST(moveCommandTest, simple)
+{
+	State state_l;
+
+	state_l.addEntity(new Entity { { 3, 3. }, 0.9, false, 1., 0});
+
+	EntityMoveCommand command_l(0, { {4, 3}, {4, 4}});
+
+	///
+	/// Step 1
+	///
+	Step step_l;
+
+	bool terminated_l = command_l.registerCommand(step_l, state_l);
+	EXPECT_FALSE(terminated_l);
+
+	ASSERT_EQ(1u, step_l.getEntityStep().size());
+
+	EntityStep * entStep_l = *step_l.getEntityStep().begin();
+
+	Vector expected_l {1., 0};
+	EXPECT_TRUE(expected_l == entStep_l->_move) << "expected : "<<expected_l<<" real : "<<entStep_l->_move;
+
+	apply(step_l, state_l);
+
+	// Now on 4,3
+
+	EXPECT_NEAR(4., state_l.getEntity(0)->_pos.x, 1e-5);
+	EXPECT_NEAR(3., state_l.getEntity(0)->_pos.y, 1e-5);
+
+	///
+	/// Step 2
+	///
+	Step step2_l;
+
+	terminated_l = command_l.registerCommand(step2_l, state_l);
+	EXPECT_FALSE(terminated_l);
+
+	ASSERT_EQ(1u, step2_l.getEntityStep().size());
+
+	EntityStep * entStep2_l = *step2_l.getEntityStep().begin();
+
+	Vector expected2_l {0., 1};
+	EXPECT_TRUE(expected2_l == entStep2_l->_move) << "expected : "<<expected2_l<<" real : "<<entStep2_l->_move;
+
+	apply(step2_l, state_l);
+
+	// Now on 4,4
+
+	EXPECT_NEAR(4., state_l.getEntity(0)->_pos.x, 1e-5);
+	EXPECT_NEAR(4., state_l.getEntity(0)->_pos.y, 1e-5);
+
+	///
+	/// Step 3
+	///
+	Step step3_l;
+
+	terminated_l = command_l.registerCommand(step3_l, state_l);
+	EXPECT_TRUE(terminated_l);
+
+	ASSERT_EQ(0u, step3_l.getEntityStep().size());
+}
