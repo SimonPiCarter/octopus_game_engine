72672ac5a0b041d393a3c0e716136ca4947aa948
Simon PIERRE <simon.pierre@eurodecision.com>
2023-01-18 14:41:16 +0100

    [step] fixed harvest step to update resource

--- src/octopus/src/step/unit/UnitHarvestStep.cc
+++ src/octopus/src/step/unit/UnitHarvestStep.cc
@@ -1,25 +1,55 @@
 #include "UnitHarvestStep.hh"
 #include "state/entity/Entity.hh"
 #include "state/entity/Unit.hh"
+#include "state/entity/Resource.hh"
+#include "state/player/Player.hh"
 #include "state/State.hh"
 #include "logger/Logger.hh"
 
 namespace octopus
 {
 
+void updateFromResource(State &state_p, Resource *res_p)
+{
+	if(res_p->_resource < 1e-3)
+	{
+		if(res_p->_alive)
+		{
+			updateGrid(state_p, res_p, false);
+		}
+		res_p->_alive = false;
+	}
+	else
+	{
+		if(!res_p->_alive)
+		{
+			updateGrid(state_p, res_p, false);
+		}
+		res_p->_alive = true;
+	}
+}
+
 void UnitHarvestQuantityStep::apply(State &state_p) const
 {
-	Entity * ent_l = state_p.getEntity(this->_handle);
 	Logger::getDebug() << "UnitHarvestQuantityStep :: apply " << this->_handle <<std::endl;
-	Unit * unit_l = dynamic_cast<Unit *>(ent_l);
+	Unit * unit_l = dynamic_cast<Unit *>(state_p.getEntity(this->_handle));
 	unit_l->_quantityOfResource += _qty;
+
+	Resource * res_l = dynamic_cast<Resource *>(state_p.getEntity(this->_res));
+	res_l->_resource -= _qty;
+
+	updateFromResource(state_p, res_l);
 }
 void UnitHarvestQuantityStep::revert(State &state_p) const
 {
-	Entity * ent_l = state_p.getEntity(this->_handle);
 	Logger::getDebug() << "UnitHarvestQuantityStep :: revert " << this->_handle <<std::endl;
-	Unit * unit_l = dynamic_cast<Unit *>(ent_l);
+	Unit * unit_l = dynamic_cast<Unit *>(state_p.getEntity(this->_handle));
 	unit_l->_quantityOfResource -= _qty;
+
+	Resource * res_l = dynamic_cast<Resource *>(state_p.getEntity(this->_res));
+	res_l->_resource -= _qty;
+
+	updateFromResource(state_p, res_l);
 }
 
 bool UnitHarvestQuantityStep::isNoOp() const
@@ -33,12 +63,14 @@ void UnitHarvestTypeStep::apply(State &state_p) const
 	Logger::getDebug() << "UnitHarvestTypeStep :: apply " << this->_handle <<std::endl;
 	Unit * unit_l = dynamic_cast<Unit *>(ent_l);
 	unit_l->_typeOfResource = _new;
+	unit_l->_quantityOfResource = 0.;
 }
 void UnitHarvestTypeStep::revert(State &state_p) const
 {
 	Entity * ent_l = state_p.getEntity(this->_handle);
 	Logger::getDebug() << "UnitHarvestTypeStep :: revert " << this->_handle <<std::endl;
 	Unit * unit_l = dynamic_cast<Unit *>(ent_l);
+	unit_l->_quantityOfResource = _oldVal;
 	unit_l->_typeOfResource = _old;
 }
 
@@ -53,6 +85,7 @@ void UnitHarvestDropStep::apply(State &state_p) const
 	Logger::getDebug() << "UnitHarvestDropStep :: apply " << this->_handle <<std::endl;
 	Unit * unit_l = dynamic_cast<Unit *>(ent_l);
 	unit_l->_quantityOfResource -= _qty;
+	state_p.getPlayer(unit_l->_player)->_resources[unit_l->_typeOfResource] += _qty;
 }
 
 void UnitHarvestDropStep::revert(State &state_p) const
@@ -61,6 +94,7 @@ void UnitHarvestDropStep::revert(State &state_p) const
 	Logger::getDebug() << "UnitHarvestDropStep :: revert " << this->_handle <<std::endl;
 	Unit * unit_l = dynamic_cast<Unit *>(ent_l);
 	unit_l->_quantityOfResource += _qty;
+	state_p.getPlayer(unit_l->_player)->_resources[unit_l->_typeOfResource] -= _qty;
 }
 
 bool UnitHarvestDropStep::isNoOp() const
--- src/octopus/src/step/unit/UnitHarvestStep.hh
+++ src/octopus/src/step/unit/UnitHarvestStep.hh
@@ -11,8 +11,8 @@ namespace octopus
 class UnitHarvestQuantityStep : public Steppable
 {
 public:
-	UnitHarvestQuantityStep(Handle const &handle_p, double const &qty_p)
-		: _handle(handle_p) , _qty(qty_p) {}
+	UnitHarvestQuantityStep(Handle const &handle_p, Handle const &res_p, double const &qty_p)
+		: _handle(handle_p) , _res(res_p), _qty(qty_p) {}
 
 	virtual void apply(State &state_p) const override;
 	virtual void revert(State &state_p) const override;
@@ -20,14 +20,15 @@ public:
 	virtual bool isNoOp() const override;
 
 	Handle _handle {0};
+	Handle _res {0};
 	double _qty {0};
 };
 
 class UnitHarvestTypeStep : public Steppable
 {
 public:
-	UnitHarvestTypeStep(Handle const &handle_p, ResourceType old_p, ResourceType new_p)
-		: _handle(handle_p) , _old(old_p) , _new(new_p) {}
+	UnitHarvestTypeStep(Handle const &handle_p, double oldVal_p, ResourceType old_p, ResourceType new_p)
+		: _handle(handle_p) , _oldVal(oldVal_p), _old(old_p) , _new(new_p) {}
 
 	virtual void apply(State &state_p) const override;
 	virtual void revert(State &state_p) const override;
@@ -35,6 +36,7 @@ public:
 	virtual bool isNoOp() const override;
 
 	Handle _handle {0};
+	double _oldVal {0.};
 	ResourceType _old {ResourceType::Food};
 	ResourceType _new {ResourceType::Food};
 };
