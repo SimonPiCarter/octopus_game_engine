895a7414aeac55ab06109a5d31bd6181440b16c7
Simon PIERRE <simon.pierre@eurodecision.com>
2023-01-18 14:39:24 +0100

    factorization

--- src/octopus/src/step/entity/EntityHitPointChangeStep.cc
+++ src/octopus/src/step/entity/EntityHitPointChangeStep.cc
@@ -5,27 +5,33 @@
 
 namespace octopus
 {
+
+void updateFromHp(State &state_p, Entity *ent_p)
+{
+	if(ent_p->_hp < 1e-3)
+	{
+		if(ent_p->_alive)
+		{
+			updateGrid(state_p, ent_p, false);
+		}
+		ent_p->_alive = false;
+	}
+	else
+	{
+		if(!ent_p->_alive)
+		{
+			updateGrid(state_p, ent_p, false);
+		}
+		ent_p->_alive = true;
+	}
+}
+
 void EntityHitPointChangeStep::apply(State &state_p) const
 {
 	Entity * ent_l = state_p.getEntity(this->_handle);
 	Logger::getDebug() << "EntityHitPointChangeStep :: " << ent_l->_hp << " + "<<this->_delta<<std::endl;
 	ent_l->_hp += this->_delta;
-	if(ent_l->_hp < 1e-3)
-	{
-		if(ent_l->_alive)
-		{
-			updateGrid(state_p, ent_l, false);
-		}
-		ent_l->_alive = false;
-	}
-	else
-	{
-		if(!ent_l->_alive)
-		{
-			updateGrid(state_p, ent_l, false);
-		}
-		ent_l->_alive = true;
-	}
+	updateFromHp(state_p, ent_l);
 }
 
 void EntityHitPointChangeStep::revert(State &state_p) const
@@ -33,22 +39,7 @@ void EntityHitPointChangeStep::revert(State &state_p) const
 	Entity * ent_l = state_p.getEntity(this->_handle);
 	Logger::getDebug() << "EntityHitPointChangeStep :: " << ent_l->_hp << " - "<<this->_delta<<std::endl;
 	ent_l->_hp -= this->_delta;
-	if(ent_l->_hp < 1e-3)
-	{
-		if(ent_l->_alive)
-		{
-			updateGrid(state_p, ent_l, true);
-		}
-		ent_l->_alive = false;
-	}
-	else
-	{
-		if(!ent_l->_alive)
-		{
-			updateGrid(state_p, ent_l, false);
-		}
-		ent_l->_alive = true;
-	}
+	updateFromHp(state_p, ent_l);
 }
 
 bool EntityHitPointChangeStep::isNoOp() const
