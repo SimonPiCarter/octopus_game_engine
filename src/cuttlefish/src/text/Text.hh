35c12d677b3ade8cfef8c330d97623aba680353f
Simon PIERRE <simon.pierre@eurodecision.com>
2023-01-30 09:39:38 +0100

    added stringify to Text

--- src/cuttlefish/src/text/Text.hh
+++ src/cuttlefish/src/text/Text.hh
@@ -2,13 +2,23 @@
 #define __Text__
 
 #include <string>
+#include <sstream>
 
 class SDL_Color;
 
 namespace cuttlefish
 {
 	class Window;
+
 	void displayText(Window* window_p, std::string const &text_p, SDL_Color const &color_p, int x, int y);
+
+	template<typename T>
+	std::string stringify(T const &t)
+	{
+		std::stringstream ss_l;
+		ss_l<<t;
+		return ss_l.str();
+	}
 } // namespace cuttlefish
 
 
