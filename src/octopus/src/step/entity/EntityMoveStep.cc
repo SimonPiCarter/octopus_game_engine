ed359f668f18a8f14115c189a3ba81c03d29eab3
Simon PIERRE <simon.pierre@eurodecision.com>
2023-01-30 17:54:14 +0100

    [step] fixed no op for move to be more precise

--- src/octopus/src/step/entity/EntityMoveStep.cc
+++ src/octopus/src/step/entity/EntityMoveStep.cc
@@ -52,7 +52,7 @@ EntityMoveStep createEntityMoveStep(Entity const &ent_p, Vector const &target_p,
 
 bool isEntityMoveStepNoOp(EntityMoveStep const &step_p)
 {
-	return square_length(step_p._move) < 1e-3;
+	return square_length(step_p._move) < 1e-5;
 }
 
 } // namespace octopus
