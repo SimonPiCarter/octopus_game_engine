657e2947daf4fca44b7af53234870ba3a798472b
Simon PIERRE <simon.pierre@eurodecision.com>
2023-02-08 19:28:17 +0100

    [octopus] added divinity

--- src/octopus/src/state/DivinityType.hh
+++ src/octopus/src/state/DivinityType.hh
@@ -6,7 +6,14 @@ namespace octopus
 	enum class DivinityType
 	{
 		Divinity_1,
-		Divinity_2
+		Divinity_2,
+		Divinity_3,
+		Divinity_4,
+		Divinity_5,
+		Divinity_6,
+		Divinity_7,
+		Divinity_8,
+		Divinity_9
 	};
 } // namespace octopus
 
