db1049c60a4e885d7a545b83376a314d3392e16d
Simon PIERRE <simon.pierre@eurodecision.com>
2023-03-01 22:41:35 +0100

    [test] fixed tests (some)

--- src/octopus/test/src/moveCommand.test.cc
+++ src/octopus/test/src/moveCommand.test.cc
@@ -30,7 +30,7 @@ TEST(moveCommandTest, simple)
 	octopus::EntityModel unitModel_l { false, 0.9, 1., 10. };
 	state_l.addEntity(new Entity { { 3, 3. }, false, unitModel_l});
 
-	EntityMoveCommand command_l(0, 0, {4, 4}, 0, { {4, 3}, {4, 4}});
+	EntityMoveCommand command_l(0, 0, {4, 3}, 0, { {4, 3}});
 
 	state_l.getEntity(0)->enqueue(&command_l, false);
 	MoveData *data_l = dynamic_cast<MoveData *>(state_l.getEntity(0)->getFrontQueue()._data);
@@ -64,21 +64,16 @@ TEST(moveCommandTest, simple)
 	Step step2_l;
 
 	terminated_l = command_l.applyCommand(step2_l, state_l, data_l);
-	EXPECT_FALSE(terminated_l);
+	EXPECT_TRUE(terminated_l);
 
-	ASSERT_EQ(1u, step2_l.getEntityMoveStep().size());
-
-	EntityMoveStep * entStep2_l = *step2_l.getEntityMoveStep().begin();
-
-	Vector expected2_l {0., 1};
-	EXPECT_TRUE(expected2_l == entStep2_l->_move) << "expected : "<<expected2_l<<" real : "<<entStep2_l->_move;
+	ASSERT_EQ(0u, step2_l.getEntityMoveStep().size());
 
 	apply(step2_l, state_l);
 
-	// Now on 4,4
+	// Still on 4,3
 
 	EXPECT_NEAR(4., state_l.getEntity(0)->_pos.x.to_double(), 1e-5);
-	EXPECT_NEAR(4., state_l.getEntity(0)->_pos.y.to_double(), 1e-5);
+	EXPECT_NEAR(3., state_l.getEntity(0)->_pos.y.to_double(), 1e-5);
 
 	///
 	/// Step 3
