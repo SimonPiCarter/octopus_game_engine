53cc38f852989d0954b60e937400d3838fb07660
Simon PIERRE <simon.pierre@eurodecision.com>
2023-03-04 10:05:09 +0100

    [cuttlefish] added progress bar

---
+++ src/cuttlefish/src/progressBar/ProgressBar.hh
@@ -0,0 +1,19 @@
+#ifndef __ProgressBar__
+#define __ProgressBar__
+
+namespace cuttlefish
+{
+
+/// @brief this class is aimed at representing a progress bar
+/// it is composed of two Picture
+/// - One for the background
+/// - Another for the progress marker
+class ProgressBar
+{
+
+};
+
+} // namespace cuttlefish
+
+
+#endif
