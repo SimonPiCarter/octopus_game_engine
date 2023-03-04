e1693aafd9d70f6d44fce4b5caee7d478022d226
SimonPiCarter <xx@gmail.com>
2023-03-04 15:07:47 +0100

    [octopus] added compiled out logger

--- src/octopus/src/logger/Logger.hh
+++ src/octopus/src/logger/Logger.hh
@@ -7,6 +7,7 @@
 
 namespace octopus
 {
+
 	/// @brief Logger handler used to log
 	/// it handles mutex
 	class LoggerHandler : public std::ostringstream
@@ -22,11 +23,23 @@ namespace octopus
 		bool _shouldLog;
 	};
 
+	class LoggerVoider {
+	public:
+		explicit LoggerVoider(std::mutex &, bool) {}
+		LoggerVoider(LoggerHandler const &) {}
+	};
+
+	/// @brief use this if you want to be able to switch from debug to normal without rebuilding (slower at execution)
+	//typedef LoggerHandler DebugLoggerHandler;
+
+	/// @brief use this if you want fastest execution (but debug log wont be possible)
+	typedef LoggerVoider DebugLoggerHandler;
+
 	class Logger
 	{
 	public:
 		static LoggerHandler getNormal();
-		static LoggerHandler getDebug();
+		static DebugLoggerHandler getDebug();
 
 		static void enable_debug() { _debug = true; }
 		static void disable_debug() { _debug = false; }
@@ -36,5 +49,14 @@ namespace octopus
 		static std::mutex _mutex;
 	};
 }
+
+template<typename T>
+octopus::LoggerVoider &operator<<(octopus::LoggerVoider &voider_p, T)
+{
+	return voider_p;
+}
+
+// to handle std::endl
+octopus::LoggerVoider &operator<<(octopus::LoggerVoider& voider_p, std::ostream& (*pf)(std::ostream&));
 
 #endif
