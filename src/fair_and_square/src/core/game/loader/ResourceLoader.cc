82525586c72d186382658a9f55a69e251c250b2d
SimonPiCarter <xx@gmail.com>
2023-03-06 21:52:08 +0100

    [fas] new panel

--- src/fair_and_square/src/core/game/loader/ResourceLoader.cc
+++ src/fair_and_square/src/core/game/loader/ResourceLoader.cc
@@ -7,14 +7,14 @@ namespace fas
 {
 RessourceLoader::RessourceLoader(cuttlefish::Window &window_p, unsigned long fullWorldSize_p, cuttlefish::World &world_p)
 	: _panel(&window_p, window_p.getWidth()-261, window_p.getHeight()-261,
-		window_p.loadTexture("resources/background.png"), window_p.loadTexture("resources/fair_and_square/grid.png"), 4)
+		window_p.loadTexture("resources/fair_and_square/panel.png"), window_p.loadTexture("resources/fair_and_square/grid.png"), 4)
 	, _statsPanel(&window_p, window_p.getWidth()-522, window_p.getHeight()-261,
-		window_p.loadTexture("resources/background.png"), window_p.loadTexture("resources/fair_and_square/grid.png"),
+		window_p.loadTexture("resources/fair_and_square/panel.png"), window_p.loadTexture("resources/fair_and_square/grid.png"),
 		window_p.loadTexture("resources/fair_and_square/prod_bar_back.png"), window_p.loadTexture("resources/fair_and_square/prod_bar_fill.png"), 4, world_p.getSelection())
 	, _divPanel(&window_p, window_p.getWidth()-window_p.getWidth()/2-200, window_p.getHeight()-window_p.getHeight()/2-200,
-		window_p.loadTexture("resources/background.png"), window_p.loadTexture("resources/fair_and_square/grid.png"), 0)
+		window_p.loadTexture("resources/fair_and_square/panel.png"), window_p.loadTexture("resources/fair_and_square/grid.png"), 0)
 	, _descPanel(&window_p, window_p.getWidth()-260, window_p.getHeight()-520,
-		window_p.loadTexture("resources/background.png"))
+		window_p.loadTexture("resources/fair_and_square/panel.png"))
 	, _spriteLib()
 	, _tilemap(fullWorldSize_p, _spriteLib, "tiles", "details")
 	, _minimap(window_p, 0, window_p.getHeight()-250, 250, 250, {"resources/me.png", "resources/enemy.png", "resources/ally.png"})
