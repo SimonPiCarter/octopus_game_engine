7fe76989294ba7ccb4af8af95d71b0834a7d0b7e
SimonP <SimonP@gmail.com>
2023-05-02 00:06:54 +0200

    added handling of constraint position steps

--- src/godoctopus/src/controller/ControllerStepVisitor.h
+++ src/godoctopus/src/controller/ControllerStepVisitor.h
@@ -78,7 +78,9 @@ public:
 	virtual void visit(octopus::ProductionPaidStep const *) override {}
 	virtual void visit(octopus::ProductionProgressionStep const *) override {}
 	virtual void visit(octopus::ResourceSpawnStep const *step_p) override;
+	virtual void visit(octopus::StateAddConstraintPositionStep const *) override {}
 	virtual void visit(octopus::StateDrawStep const *) override {}
+	virtual void visit(octopus::StateRemoveConstraintPositionStep const *) override {}
 	virtual void visit(octopus::StateTemplePositionAddStep const *) override {}
 	virtual void visit(octopus::StateTemplePositionRemoveStep const *) override {}
 	virtual void visit(octopus::StateWinStep const *) override {}
