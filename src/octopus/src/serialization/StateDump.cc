524b6b9e028b6c3d23b0a120e82c594da9598509
SimonP <SimonP@gmail.com>
2023-05-29 09:42:03 +0200

    [octopus] added text state dump

---
+++ src/octopus/src/serialization/StateDump.cc
@@ -0,0 +1,35 @@
+#include "StateDump.hh"
+
+#include "state/State.hh"
+#include "state/entity/Entity.hh"
+#include "state/player/Player.hh"
+
+namespace octopus
+{
+void streamStateAsSimpleText(std::ostream &os_p, State const &state_p)
+{
+    for(Player const *player_l : state_p.getPlayers())
+    {
+        os_p<<"Player "<<player_l->_id<<std::endl;
+        os_p<<"Resources"<<std::endl;
+        for(auto &&pair_l : player_l->_resources)
+        {
+            os_p<<"\t"<<pair_l.first<<" : "<<pair_l.second.data()<<std::endl;
+        }
+        os_p<<"Upgrades"<<std::endl;
+        for(auto &&pair_l : player_l->_upgradeLvl)
+        {
+            os_p<<"\t"<<pair_l.first<<" : "<<pair_l.second<<std::endl;
+        }
+    }
+    for(Entity const * ent_l : state_p.getEntities())
+    {
+        os_p<<"Entity "<<ent_l->_handle<<std::endl;
+        os_p<<"\talive "<<(ent_l->_alive?"1":"0")<<std::endl;
+        os_p<<"\thp "<<ent_l->_hp<<std::endl;
+        os_p<<"\tpos "<<ent_l->_pos.x.data()<<","<<ent_l->_pos.y.data()<<std::endl;
+    }
+}
+
+} // namespace octopus
+
---
+++ src/octopus/src/serialization/StateDump.hh
@@ -0,0 +1,25 @@
+#ifndef __StateDump__
+#define __StateDump__
+
+#include <iostream>
+
+namespace octopus
+{
+
+class State;
+
+/// @brief write state information as simple text to the given output stream
+/// will stream information
+/// - Players
+///   - Amount of resources
+///   - upgrade level
+/// - Entities
+///   - Hit point
+///   - Position
+///   - Alive (1/0)
+void streamStateAsSimpleText(std::ostream &os_p, State const &state_p);
+
+} // namespace octopus
+
+
+#endif
