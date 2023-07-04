168ffb71b4c3537a656d8af56bff9c34c8ca2d14
SimonPiCarter <xx@gmail.com>
2023-07-04 22:55:36 +0200

    [fas] added tier upgrade template

--- src/fair_and_square/src/library/model/divinity/generators/UnitModelIds.hh
+++ src/fair_and_square/src/library/model/divinity/generators/UnitModelIds.hh
@@ -1,8 +1,11 @@
 #ifndef __UnitModelsId__
 #define __UnitModelsId__
 
+#include <functional>
 #include <string>
 #include <vector>
+
+#include "state/player/upgrade/StepUpgradeGenerator.hh"
 
 namespace models
 {
@@ -50,6 +53,56 @@ namespace models
 
     const std::vector<std::string> BasicBuildingDepositModels = {
     };
+
+    template<typename param_t>
+    class TierUpgrade : public octopus::StepUpgradeGenerator
+    {
+    public:
+        TierUpgrade(param_t const &param_p, std::function<std::vector<octopus::Steppable *>(param_t, unsigned long)> const &fn_p) : _param(param_p), _fn(fn_p) {}
+
+        virtual TierUpgrade* newCopy() const override
+        {
+            return new TierUpgrade(_param, _fn);
+        }
+
+        virtual std::vector<octopus::Steppable *> getSteppables(unsigned long player_p) const
+        {
+            return _fn(_param, player_p);
+        }
+    private:
+        param_t const _param;
+        std::function<std::vector<octopus::Steppable *>(param_t, unsigned long)> const _fn;
+    };
+
+    template<typename param_t>
+    std::vector<std::string> fillTierUpgrade(octopus::Library &lib_p, param_t const &params_p, std::string const &id_p,
+        std::function<std::vector<octopus::Steppable *>(param_t, unsigned long)> const &t1_p,
+        std::function<std::vector<octopus::Steppable *>(param_t, unsigned long)> const &t2_p,
+        std::function<std::vector<octopus::Steppable *>(param_t, unsigned long)> const &t3_p)
+    {
+	    Upgrade * upT1_l = new Upgrade(id_p+"_UpgradeTierOne", new models::TierUpgrade<param_t>(params_p, t1_p));
+        upT1_l->_cost["bloc"] = 200;
+        upT1_l->_cost["ether"] = 100;
+        upT1_l->_productionTime = 6000;
+        upT1_l->_requirements._upgradeLvl[id_p] = 1;
+	    lib_p.registerUpgrade(upT1_l->_id, upT1_l);
+
+	    Upgrade * upT2_l = new Upgrade(id_p+"_UpgradeTierTwo", new models::TierUpgrade<param_t>(params_p, t2_p));
+        upT2_l->_cost["bloc"] = 600;
+        upT2_l->_cost["ether"] = 300;
+        upT2_l->_productionTime = 12000;
+        upT2_l->_requirements._upgradeLvl[id_p] = 2;
+	    lib_p.registerUpgrade(upT2_l->_id, upT2_l);
+
+	    Upgrade * upT3_l = new Upgrade(id_p+"_UpgradeTierThree", new models::TierUpgrade<param_t>(params_p, t2_p));
+        upT3_l->_cost["bloc"] = 1200;
+        upT3_l->_cost["ether"] = 600;
+        upT3_l->_productionTime = 12000;
+        upT3_l->_requirements._upgradeLvl[id_p] = 3;
+	    lib_p.registerUpgrade(upT3_l->_id, upT3_l);
+
+        return {upT1_l->_id, upT2_l->_id, upT3_l->_id};
+    }
 }
 
 #endif
