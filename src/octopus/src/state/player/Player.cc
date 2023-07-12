788af72f43a47b14b8ddc1802186636da51297f8
SimonPiCarter <xx@gmail.com>
2023-07-12 23:51:56 +0200

    [octopus] fixed chedck requirement not being correctly used

--- src/octopus/src/state/player/Player.cc
+++ src/octopus/src/state/player/Player.cc
@@ -120,8 +120,9 @@ std::list<Upgrade const *> getAvailableUpgrades(BuildingModel const &building_p,
 	for(Upgrade const * upgrade_l : building_p._upgrades)
 	{
 		// meet requirement and avoid repeat upgrade
-		if(meetRequirements(upgrade_l->_requirements, player_p) || !checkRequirements_p
+		if((meetRequirements(upgrade_l->_requirements, player_p)
 		&& (upgrade_l->_repeatable || player_p._producedUpgrade.find(upgrade_l->_id) == player_p._producedUpgrade.end()))
+		|| !checkRequirements_p)
 		{
 			availables_l.push_back(upgrade_l);
 		}
