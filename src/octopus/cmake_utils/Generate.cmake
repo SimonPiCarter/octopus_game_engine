f76ddd434c5c826c611aa20490905dc6fd010fb9
SimonPiCarter <xxx@gmail.com>
2023-09-18 10:05:02 +0200

    [cmake] improved versoin update

--- src/octopus/cmake_utils/Generate.cmake
+++ src/octopus/cmake_utils/Generate.cmake
@@ -1,7 +1,4 @@
 
-# routine to make it update
-EXECUTE_PROCESS(COMMAND ${CMAKE_BINARY_DIR}/RetreiveDateTime OUTPUT_VARIABLE DATETIMESTRING)
-MESSAGE(STATUS "DATETIME=\"${DATETIMESTRING}\"")
 
 # ============================================================================
 # Version management
@@ -9,14 +6,16 @@ MESSAGE(STATUS "DATETIME=\"${DATETIMESTRING}\"")
 
 find_package(Git REQUIRED)
 execute_process(
-   COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
-   RESULT_VARIABLE result
-   OUTPUT_VARIABLE GIT_HASH
-   OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+	COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
+	RESULT_VARIABLE result
+	OUTPUT_VARIABLE GIT_HASH
+	OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
 
 if(result)
     message(FATAL_ERROR "Failed to get git hash: ${result}")
 endif()
+
+MESSAGE(STATUS "Updated version =\"${GIT_HASH}\"")
 
 configure_file(Version.cc.in Version.cc @ONLY)
