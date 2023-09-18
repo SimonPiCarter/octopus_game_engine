872057071a5e322aa1caf99206cd326030a51bfb
SimonPiCarter <xxx@gmail.com>
2023-09-18 10:06:08 +0200

    [octopus] removed double def

--- src/octopus/src/state/entity/attackModifier/AttackModifier.cc
+++ src/octopus/src/state/entity/attackModifier/AttackModifier.cc
@@ -9,18 +9,6 @@
 namespace octopus
 {
 
-void applyMainAttack(Step &step_p, AttackModifierData const &data_p, State const &state_p)
-{
-	if(state_p.isEntityAlive(data_p.target))
-	{
-		Entity const & target_l = *state_p.getLoseEntity(data_p.target.index);
-		Fixed curHp_l = target_l._hp + step_p.getHpChange(data_p.target);
-		Fixed maxHp_l = target_l.getHpMax();
-		Fixed dmg_l = std::min(Fixed(-1), target_l.getArmor() - data_p.bonusDamage);
-		step_p.addSteppable(new EntityHitPointChangeStep(target_l._handle, dmg_l, curHp_l, maxHp_l));
-	}
-}
-
 void NoModifier::newAttackSteppable(Step &step_p, AttackModifierData const &data_p, State const &state_p, bool) const
 {
 	applyMainAttack(step_p, data_p, state_p);
