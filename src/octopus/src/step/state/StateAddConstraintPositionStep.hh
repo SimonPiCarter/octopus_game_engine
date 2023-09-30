3bfbe0d984485ce0766970f91ad6e64d169d3ddc
SimonPiCarter <xxx@gmail.com>
2023-09-30 11:23:15 +0200

    [octopus] added comments

--- src/octopus/src/step/state/StateAddConstraintPositionStep.hh
+++ src/octopus/src/step/state/StateAddConstraintPositionStep.hh
@@ -7,9 +7,17 @@
 
 namespace octopus
 {
+/// @brief spawn a position constraint
 class StateAddConstraintPositionStep : public Steppable
 {
 public:
+	/// @brief spawn a position constraint
+	/// @param player_p the player that will be constrained
+	/// @param val_p this is the value of the position that will be constrained (x or y)
+	/// @param min_p this is the the min position (y or x) where the constraint will apply
+	/// @param max_p this is the the max position (y or x) where the constraint will apply
+	/// @param less_p tell if the constraint constraint the position to be less than the value of more
+	/// @param x_p tells if the val_p is a x (if true) or y value (min and max will be the opposite)
 	StateAddConstraintPositionStep(unsigned long player_p, Fixed const &val_p, Fixed const &min_p, Fixed const &max_p, bool less_p, bool x_p)
 		: _player(player_p), _val(val_p), _min(min_p), _max(max_p), _less(less_p), _x(x_p)
 	{}
