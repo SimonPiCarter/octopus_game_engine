9455e8fde053abaad957442af9c155f2cc253348
Simon <you@example.com>
2023-12-22 20:56:38 +0100

    usage of version if no git success

--- src/octopus/cmake_utils/Generate.cmake
+++ src/octopus/cmake_utils/Generate.cmake
@@ -4,16 +4,21 @@
 # Version management
 # ============================================================================
 
-find_package(Git REQUIRED)
-execute_process(
-	COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
-	RESULT_VARIABLE result
-	OUTPUT_VARIABLE GIT_HASH
-	OUTPUT_STRIP_TRAILING_WHITESPACE
-	)
+find_package(Git)
+
+if(Git_FOUND)
+	execute_process(
+		COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
+		RESULT_VARIABLE result
+		OUTPUT_VARIABLE GIT_HASH
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+		)
+else()
+	set(result 1)
+endif()
 
 if(result)
-    message(FATAL_ERROR "Failed to get git hash: ${result}")
+	set(GIT_HASH ${PROJECT_VERSION})
 endif()
 
 MESSAGE(STATUS "Updated version =\"${GIT_HASH}\"")
